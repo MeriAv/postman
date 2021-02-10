921f0e76b9dc6e8f32c21093fca65bdabaf29ca0
MeriAv <mari.avdeeva1407@gmal.com>
2021-02-10 17:18:08 +0300

    test fail

---
+++ src/test/java/ru/netology/EchoPostmanTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class EchoPostmanTest {
+
+    @Test
+    void shouldSendData() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+
+
+}
